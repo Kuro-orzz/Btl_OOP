886e812a1d062ad119d3f96e7092e511fce5b65f
luupin141 <ducluutk@gmail.com>
2024-11-05 13:51:11 +0700

    ReOrganize code && Handle LogIn action

--- Code/Login.java
+++ Code/Login.java
@@ -1,6 +1,5 @@
 package Code;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,33 +8,33 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-public class Login extends Application {
-    public void start(Stage stage) {
-        // Background
-        LoadImage LOGIN_IMG = new LoadImage();
+public class Login {
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password";
 
-        // Text "LOGIN"
-        Label LOGIN = new Label("Login");
-        LOGIN.setStyle("-fx-font-size: 100px");
-        LOGIN.setTranslateX(0);
-        LOGIN.setTranslateY(-170);
+    public Scene getLoginScene(MainApp mainApp) {
+
+        LoadImage loginImageLoader = new LoadImage();
+
+        Label loginLabel = new Label("Login");
+        loginLabel.setStyle("-fx-font-size: 100px");
+        loginLabel.setTranslateY(-170);
 
         // Enter Username
-        TextField loginField = new TextField();
-        loginField.setPromptText("Enter Username");
-        loginField.setStyle("-fx-background-color: white; -fx-opacity: 0.8; -fx-font-size: 16px; -fx-prompt-text-fill: gray;");
-        loginField.setPrefWidth(200);
-        loginField.setPrefHeight(40);
-        loginField.textProperty().addListener((observable, oldValue, newValue) -> {
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("Enter Username");
+        usernameField.setStyle("-fx-background-color: white; -fx-opacity: 0.8; -fx-font-size: 16px; -fx-prompt-text-fill: gray;");
+        usernameField.setPrefWidth(200);
+        usernameField.setPrefHeight(40);
+        usernameField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue.length() > 40) {
                 // only get 40 character
-                loginField.setText(newValue.substring(0, 40));
+                usernameField.setText(newValue.substring(0, 40));
             }
         });
         Pane pane1 = new Pane();
-        pane1.getChildren().addAll(loginField);
+        pane1.getChildren().addAll(usernameField);
         pane1.setTranslateX(540);
         pane1.setTranslateY(300);
 
@@ -51,6 +50,7 @@ public class Login extends Application {
                 passwordField.setText(newValue.substring(0, 40));
             }
         });
+
         Pane pane2 = new Pane();
         pane2.getChildren().addAll(passwordField);
         pane2.setTranslateX(540);
@@ -59,33 +59,28 @@ public class Login extends Application {
         VBox vbox = new VBox();
         vbox.getChildren().addAll(pane1, pane2);
 
-        // create button
         Button loginButton = new Button("Login");
-        loginButton.setStyle("-fx-font-size: 16px; -fx-background-color: #4CAF50; -fx-text-fill: white;");
-        loginButton.setPrefWidth(100);
-        loginButton.setPrefHeight(40);
-        loginButton.setTranslateX(0);
         loginButton.setTranslateY(100);
-
         loginButton.setOnAction(event -> {
-            String username = loginField.getText();
-            String password = passwordField.getText();
-            System.out.println("Username: " + username);
-            System.out.println("Password: " + password);
+            if (authenticate(usernameField.getText(), passwordField.getText())) {
+                mainApp.showSidebarScene();
+            } else {
+                System.out.println("Invalid Account!");
+            }
         });
 
-        // Add all to stack pane
-        StackPane stpane = new StackPane();
-        stpane.getChildren().addAll(LOGIN_IMG.loadLoginImage(), LOGIN, vbox, loginButton);
-
-        // show all element to screen
-        Scene scene = new Scene(stpane, 1280, 720);
-        stage.setTitle("Login");
-        stage.setScene(scene);
-        stage.show();
+        StackPane stPane = new StackPane();
+        stPane.getChildren().addAll(loginImageLoader.loadLoginImage(), loginLabel, vbox, loginButton);
+        return new Scene(stPane, 1280, 720);
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    /**
+     * Check if an account with username and password is valid.
+     * @param username username of an account logging in
+     * @param password password of an account logging in
+     * @return true if account is available and false if unavailable
+     */
+    private boolean authenticate(String username, String password) {
+        return USERNAME.equals(username) && PASSWORD.equals(password);
     }
 }
